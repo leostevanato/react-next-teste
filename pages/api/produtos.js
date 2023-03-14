59dadd9dafa161dc3460f6817c9abff0366231d4
leostevanato <47180899+leostevanato@users.noreply.github.com>
2023-03-14 11:39:40 -0300

    Atualizar CRUD de produtos - Implementar o que faltava do CRUD. - Renomear tabelas e campos do banco. - Criar e reorganizar arquivos. - Ao clicar em SAVE para criar, apagar o campo. - Habilitar botão SAVE se o campo não estiver vazio. - Atualizar o layout.

--- pages/api/produtos.js
+++ pages/api/produtos.js
@@ -5,28 +5,32 @@ export default async function handler(req, res) {
 
 	if (req.method === "GET") {
 		const products = await query({
-			query: "SELECT * FROM produtos",
+			query: "SELECT * FROM products",
 			values: [],
 		});
 		res.status(200).json({ products: products });
 	}
 
 	if (req.method === "POST") {
-		const productName = req.body.produto_nome;
+		const productName = req.body.product_name;
 		const addProducts = await query({
-			query: "INSERT INTO produtos (produto_nome) VALUES (?)",
+			query: "INSERT INTO products (product_name) VALUES (?)",
 			values: [productName],
 		});
+
 		let product = [];
+
 		if (addProducts.insertId) {
 			message = "success";
 		} else {
 			message = "error";
 		}
+
 		product = {
 			product_id: addProducts.insertId,
 			product_name: productName,
 		};
+
 		res.status(200).json({ response: { message: message, product: product } });
 	}
 
@@ -34,34 +38,41 @@ export default async function handler(req, res) {
 		const productId = req.body.product_id;
 		const productName = req.body.product_name;
 		const updateProducts = await query({
-			query: "UPDATE produtos SET produto_nome = ? WHERE produto_id = ?",
+			query: "UPDATE products SET product_name = ? WHERE product_id = ?",
 			values: [productName, productId],
 		});
+
 		const result = updateProducts.affectedRows;
+
 		if (result) {
 			message = "success";
 		} else {
 			message = "error";
 		}
+
 		const product = {
 			product_id: productId,
 			product_name: productName,
 		};
+
 		res.status(200).json({ response: { message: message, product: product } });
 	}
 
 	if (req.method === "DELETE") {
 		const productId = req.body.product_id;
 		const deleteProducts = await query({
-			query: "DELETE FROM produtos WHERE produto_id = ?",
+			query: "DELETE FROM products WHERE product_id = ?",
 			values: [productId],
 		});
+
 		const result = deleteProducts.affectedRows;
+
 		if (result) {
 			message = "success";
 		} else {
 			message = "error";
 		}
+
 		res
 			.status(200)
 			.json({ response: { message: message, product_id: productId } });
